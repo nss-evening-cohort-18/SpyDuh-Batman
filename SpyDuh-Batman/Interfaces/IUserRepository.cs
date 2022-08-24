6d20e31007058b3cb2ec48d18680404df0ad6bfd
macgere <mac_gere@yahoo.com>
2022-08-23 20:55:00 -0500

    Made Interface

---
+++ SpyDuh-Batman/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using SpyDuh_Batman.Repositories;
+using SpyDuh_Batman.Models;
+
+namespace SpyDuh_Batman.Interfaces
+{
+    public interface IUserRepository
+    {
+        List<User> GetAll();
+        User? GetUserById(int id);
+        List<User>? GetUsersBySkills(string skill);
+        List<User>? GetUsersByFriendship(int Id);
+        List<User>? GetUsersByEnemies(int Id);
+        public void CreateUser(User user);
+    }
+}
