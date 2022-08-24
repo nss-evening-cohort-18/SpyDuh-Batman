f1ccdd190bbbf961ca7efd4e2da64b040c98d530
Rick Kersmarki <rkersmarki@gmail.com>
2022-08-23 19:05:56 -0500

    User Class

---
+++ SpyDuh-Batman/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace SpyDuh_Batman.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string CodeName { get; set; }
+
+        public List<int> Friends { get; set; }
+
+        public List<int> Enemies { get; set; }
+
+        public List<string> Skills { get; set; }
+
+        public bool HasSideKick { get; set; }
+
+    }
+}
